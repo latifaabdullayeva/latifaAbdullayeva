d34f091966b30f44d788b590d06b7cb678759082
Latifa <latifaabdullayeva@gmail.com>
2021-03-20 23:32:39 +0400

    Adds Button Element

---
+++ src/components/ButtonElement.js
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+import { Link } from "react-scroll";
+
+export const Button = styled(Link)`
+  border-radius: 50px;
+  background: ${({ primary }) => (primary ? "#47fdec" : "#00141c")};
+  white-space: nowrap;
+  padding: ${({ big }) => (big ? "14px 48px" : "12px 30px")};
+  color: ${({ dark }) => (dark ? "#000000" : "#fff")};
+  font-size: ${({ fontBig }) => (fontBig ? "20px" : "16px")};
+  outline: none;
+  border: none;
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  transition: all 0.2s ease-in-out;
+
+  &:hover {
+    transition: all 0.2s ease-in-out;
+    background: ${({ primary }) => (primary ? "#fff" : "#47fdec")};
+  }
+`;
+
+// #47fdec
+// #00141c
