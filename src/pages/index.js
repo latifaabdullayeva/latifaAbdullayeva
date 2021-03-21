4b6f1f9d2a387e4f47c6db4d4fb986d6bff91364
Latifa <latifaabdullayeva@gmail.com>
2021-03-21 18:49:07 +0400

    Modifies pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,17 @@
 import React, { useState } from "react";
-import Sidebar from "../components/Sidebar";
-import Navbar from "../components/Navbar";
-import HeroSection from "../components/HeroSection";
+import Sidebar from "../components/navigation/Sidebar";
+import Navbar from "../components/navigation/Navbar";
+import HomeSection from "../components/landingPage/HomeSection";
+import InfoSection from "../components/landingPage/InfoSection";
+import {
+  homeObjOne,
+  homeObjTwo,
+} from "../components/landingPage/InfoSection/Data";
+import Services from "../components/landingPage/Services";
+import {
+  homeObjThree,
+  homeObjFour,
+} from "../components/landingPage/Services/Data";
 
 const Home = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -14,7 +24,11 @@ const Home = () => {
     <>
       <Sidebar isOpen={isOpen} toggle={toggle} />
       <Navbar toggle={toggle} />
-      <HeroSection />
+      <HomeSection />
+      <InfoSection {...homeObjOne} />
+      <InfoSection {...homeObjTwo} />
+      <Services {...homeObjThree} />
+      <Services {...homeObjFour} />
     </>
   );
 };
