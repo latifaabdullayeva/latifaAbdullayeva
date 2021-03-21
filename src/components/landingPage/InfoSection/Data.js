548a4b23b9487815aa5188c5b952cc1e972984d4
Latifa <latifaabdullayeva@gmail.com>
2021-03-21 18:47:09 +0400

    Adds Information about me section to the landing page

---
+++ src/components/landingPage/InfoSection/Data.js
@@ -0,0 +1,36 @@
+export const homeObjOne = {
+  id: "about",
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: "About Me",
+  headline: "Hobbies, project experiences and more",
+  description:
+    "Here, you can learn more about me and my hobbies. I also describe briefly  my work experience on different projects.",
+  buttonLabel: "Learn More",
+  imgStart: false,
+  img: require("../../../images/myPhotoBlackWhite.png").default,
+  alt: "myProfilePhoto",
+  dark: true,
+  primary: true,
+  darkText: false,
+};
+
+export const homeObjTwo = {
+  id: "resume",
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: "Resume",
+  headline: "Education, skills, professional experience and more",
+  description:
+    "You can learn more about my skills, professional experience, education, accomplishments, additional projects and languages.",
+  buttonLabel: "Learn More",
+  imgStart: false,
+  img: require("../../../images/svg-1.svg").default,
+  alt: "projectsImg",
+  dark: true,
+  primary: true,
+  darkText: false,
+};
+
